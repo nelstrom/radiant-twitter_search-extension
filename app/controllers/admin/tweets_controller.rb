1c666139bde66335e7c05ab0b8b4b868382593d8
Andrew Neil <andrew.jr.neil@gmail.com>
2009-08-09 13:12:06 +0100

    Tweets index splits by featured.

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -2,6 +2,7 @@ class Admin::TweetsController < ApplicationController
 
   def index
     @search_term = SearchTerm.find(params[:search_term_id])
-    @tweets = @search_term.tweets
+    @featured = @search_term.tweets.all(:conditions => ["featured = ?", true])
+    @tweets = @search_term.tweets.all(:conditions => ["featured IS NULL"])
   end
 end
--- app/helpers/admin/tweets_helper.rb
+++ app/helpers/admin/tweets_helper.rb
@@ -1,2 +1,5 @@
 module Admin::TweetsHelper
+  def link_to_status_on_twitter(text, username, status_id)
+    "<a href='http://twitter.com/#{username}/status/#{status_id}'>#{text}</a>"
+  end
 end
